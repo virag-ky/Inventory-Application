a039549ea584d5ade724dda3786ce1f8db83238d
Virag Kormoczy <vkormoczy@gmail.com>
2023-04-14 15:10:36 +0300

    Set up database connection

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
 
 const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
 
 const app = express();
+const mongoDB = process.env.MONGODB_URI;
+
+const main = async () => {
+  await mongoose
+    .connect(mongoDB)
+    .then(() => console.log('connected to database'))
+    .catch((err) => console.log(err));
+};
+main();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
